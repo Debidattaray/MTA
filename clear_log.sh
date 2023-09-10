ce833df0c341be8b89296d8b2fcd8f2ca394a0ad
Debidattaray <47247317+Debidattaray@users.noreply.github.com>
2023-09-10 21:15:30 +0530

    Update clear_log.sh

--- clear_log.sh
+++ clear_log.sh
@@ -5,4 +5,4 @@ LOG_DIR=
 truncate -s 0 /etc/pmta/log/pmta.log
 truncate -s 0 /etc/pmta/log/pmta.log.1
 
-(crontab -l | grep -q "/etc/pmta/log/clear_log.sh") || (crontab -l; echo "0 */4 * * * /etc/pmta/log/clear_log.sh") | crontab -
+(crontab -l | grep -q "/apps/clear_log.sh") || (crontab -l; echo "0 */4 * * * /apps/clear_log.sh") | crontab -
